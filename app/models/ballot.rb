c805e120d0f6f09ffa0c763b9aa433f4ea4a4eb3
Noah Goetz <noahgoetz@noahs-mbp.wv.cc.cmu.edu>
2015-01-16 19:36:37 -0500

    fixed winner

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -2,5 +2,5 @@ class Ballot < ActiveRecord::Base
   belongs_to :gov, class_name: "Team"
   belongs_to :opp, class_name: "Team"
   belongs_to :judge, class_name: "User"
-  belongs_to :winner, class_name: "User"
+  belongs_to :winner, class_name: "Team"
 end
